476d57c77851a27429a6e4d2e5b4f7be311209de
Soul Inferno DE <SoulInfernoDE@users.noreply.github.com>
2022-04-30 10:20:24 +0200

    updated speedlaunch.sh
    using the standard question box (syscalll_check.sh) to disable abi.vsyscall32 now.

--- speedlaunch.sh
+++ speedlaunch.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
+# using the standard question box to enable or disable the anti cheat value abi.vsyscall32
+$(dirname "$0")/syscall_check.sh
 # Uncomment to manully set the AntiCheat value
 # You will be asked for your root password every time you launch this script
-pkexec sh -c 'sysctl -w abi.vsyscall32=0'
+# pkexec sh -c 'sysctl -w abi.vsyscall32=0'
 # Uncomment the next line to automatically set vsyscall32 to zero
 # sudo sysctl -w abi.vsyscall32=0
 # You need to add the following line to the end of: sudo visudo
